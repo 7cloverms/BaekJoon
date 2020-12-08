5bae330e98adc328c02aa8bf043f4191b5c878d9
7cloverms <67933801+7cloverms@users.noreply.github.com>
2020-12-08 17:07:33 +0900

    Update 1712.c

--- 1712.c
+++ 1712.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c, i;
+	scanf("%d %d %d", &a, &b, &c);
+    if (b >= c){
+		printf("-1");
+		return 0;
+	}
+	i = a / (c - b);
+	if (a + b*i >= c*i){
+		i += 1;
+	}
+
 	
+	printf("%d", i);
+    return 0;
+}
